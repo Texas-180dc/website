2f77c22042566ec0bb9f3e02bea281edca1d6a85
Matt Funk <mfunk017@gmail.com>
2019-11-13 17:10:04 -0600

    created project

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Contact = () => (
+    <div>
+        <h2>Contact Page</h2>
+        <p>Tellus integer feugiat scelerisque varius morbi. Et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Sapien eget mi proin sed libero enim sed. Egestas quis ipsum suspendisse ultrices gravida dictum. Egestas pretium aenean pharetra magna ac placerat. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Tellus in hac habitasse platea. Mollis nunc sed id semper risus in hendrerit. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Ut venenatis tellus in metus. Massa tincidunt dui ut ornare lectus sit amet. Facilisi morbi tempus iaculis urna id volutpat lacus. Lectus mauris ultrices eros in. Magnis dis parturient montes nascetur.</p>
+    </div>
+)
